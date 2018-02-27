12514c5f85a2734df8d57530df7428adb07bf09a
popflux <popflux@gmail.com>
2018-02-27 22:01:50 +0700

    List Item

--- src/index.js
+++ src/index.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Message(props){
-	if (props.value) {
-		return <h1>This is first message</h1>
-	}
-
-	return <h1>I am from second message</h1>
+function ColorList(props){
+	const list = props.color
+	// const clrs = list.map((value, index)=> <li key={index}>{value}</li>)
+	return <ul>{
+						list.map((value, index)=> <li key={index}>{value}</li>)
+					}</ul>
 }
 
-class Btn extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {value: true}
-	}
-
-	handleClick = () => {
-		this.setState({
-			value : !this.state.value
-		});
-	}
-
-	render(){
-		return (
-							<div>
-								<button onClick={this.handleClick}>Change the message</button>
-								<Message value={this.state.value} />
-							</div>
-						)
-	}
-}
+const colors = ['Red', 'Blue', 'Green', 'Blue']
 
 ReactDOM.render(
-	<Btn />,
+	<ColorList color={colors} />,
 	document.getElementById('root')
 );
+
